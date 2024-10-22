3959582cdf1ef70c2c1e426be2e9a5f7df628207
PC <PC@DESKTOP-8R7NOCS>
2024-10-23 00:47:31 +0300

    7. Ders eklemesi

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,82 @@
+﻿#region Dizi Örnek
+//string[] cities = { "adana", "adıyaman", "afyon", "ağrı", };
+//Console.WriteLine(cities[2]);
+#endregion
+
+#region Dizideki elemanları yazdırma
+
+//string[] cities = { "adana", "adıyaman", "afyon", "ağrı", };
+
+//for (int i = 0; i < cities.Length; i++)
+//{
+//    Console.WriteLine(cities[i]);
+//}
+
+#endregion
+
+#region Üçe tam bölünen sayılar dizisi 
+//int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (i% 3==0)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Sayı üçe tam bölünmedi.");
+//    }
+//}
+#endregion
+
+#region Dizi içindeki en büyük değeri bulma
+//int[] numbers = { 34, 35, 16, 48, 50, 55, 70, 78, 90, 3 };
+//int maxNumber = numbers[0];
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > numbers[i])
+//    {
+//        maxNumber = numbers[i];
+//    }
+//}   
+// Console.WriteLine(maxNumber);
+
+#endregion
+
+#region Sort Metodu
+//int[] numbers = { 34, 35, 16, 48, 50, 55, 70, 78, 90, 3 };
+//Array.Sort(numbers);
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+#endregion
+
+#region Min-Max Metodu
+//int[] numbers = { 34, 35, 16, 48, 50, 55, 70, 78, 90, 3 };
+//Console.WriteLine("Dizideki en nüyük eleman : " +numbers.Max()+" Dizinin en küçük elemanı : "+numbers.Min());
+#endregion
+
+#region Tek-Çift
+//int[] numbers = { 34, 35, 16, 48, 50, 55, 70, 78, 90, 3 };
+//Console.WriteLine("Çift Sayılar");
+//for (int i = 0;i<numbers.Length;i++)
+//{
+//    if (numbers[i] % 2 == 0)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+
+//}
+//Console.WriteLine("Tek Sayılar");
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] % 2 == 1)
+//    {
+//        Console.WriteLine(numbers[i]);
+//    }
+
+//}
+
+#endregion
+
